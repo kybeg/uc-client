4bc8690b0814bc398b76d939add6c64577cdc102
Kyrre <kyrre@begnum.no>
2023-01-04 10:25:18 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+# UC client installation script
+
+OLD_DIRECTORY=$(pwd)
+
+# CD into a teporary folder
+cd /tmp
+
+# clone the repository
+
+git clone https://github.com/kybeg/uc-client.git
+
+# copy the uc script to /usr/local/bin
+
+if cp uc-client/uc /usr/local/bin/uc; then
+
+# remove the repo
+
+rm -r uc-client
+
+# go back to original folder
+
+cd $OLD_DIRECTORY
+
+echo -e "\nuc client installed"
+
+else
+
+echo -e "\nSomething went wrong with the installation"
+
+fi
